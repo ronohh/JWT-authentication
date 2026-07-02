23b2b242980ede2f5c4337528975f3fdf05db0a2
Ronohh <cimanu88@gmail.com>
2026-07-02 20:33:32 +0300

    authenticating users

---
+++ server.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+
+const express = require("express");
+const app = express();
+
+const jwt = require('jsonwebtoken')
+app.use(express.json())
+
+const posts = [
+    {
+        username: 'jack',
+        title: 'post 1'
+    },
+    {
+        username: 'daniels',
+        title: 'post 2'
+    }
+]
+
+app.get('/posts', (req,res) => {
+    res.json(posts)
+})
+
+app.post('/login', (req,res) => {
+    //Authenticate User
+    const username = req.body.username
+    const user = { name: username}
+    const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET)
+    res.json({ accessToken: accessToken })
+})
+
+app.listen(3000)
