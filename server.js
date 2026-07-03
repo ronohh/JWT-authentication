179c4313db45c450c314e5c9d68cf3b62adc99a5
Ronohh <cimanu88@gmail.com>
2026-07-03 11:12:07 +0300

    middleware

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 
 const jwt = require('jsonwebtoken')
+
 app.use(express.json())
 
 const posts = [
@@ -17,8 +18,9 @@ const posts = [
     }
 ]
 
-app.get('/posts', (req,res) => {
-    res.json(posts)
+app.get('/posts',authenticateToken, (req,res) => {
+
+    res.json(posts.filter(post => post.username === req.user.name))
 })
 
 app.post('/login', (req,res) => {
@@ -28,5 +30,17 @@ app.post('/login', (req,res) => {
     const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET)
     res.json({ accessToken: accessToken })
 })
+
+function authenticateToken(req,res,next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split('')[1]
+    if (token == null ) return res.sendStatus(401)
+
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+            if (err) return res.sendStatus(403)
+                req.user = user
+            next()
+        })
+}
 
 app.listen(3000)
